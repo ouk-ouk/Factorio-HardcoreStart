6342b6eed96d665f1136c75566b422e7c2653695
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2021-02-04 18:39:11 +0100

    First version for Factorio 1.0.x.

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,4 @@
-script.on_event(defines.events.on_player_created, function(event)
+script.on_event(defines.events.on_cutscene_cancelled, function(event)
   local player = game.players[event.player_index]
   if player.character then
     player.character.clear_items_inside()
